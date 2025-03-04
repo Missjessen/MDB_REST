15e0dfa301b5664f66a3a818c4fa48c7c9a6d694
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-03-04 12:22:39 +0100

    Fix package.json and TypeScript build

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,8 @@ const app: Application = express();
 function corsSetup() {
     
 app.use(cors({
-    origin: "http://localhost:5173",  // Tillad requests fra din frontend
-    //  origin: '*',  // Tillad requests fra din frontend
+   // origin: "http://localhost:5173",  // Tillad requests fra din frontend
+    origin: '*',  // Tillad requests fra din frontend
     methods: "GET,POST,PUT,DELETE,OPTIONS, HEAD", 
     allowedHeaders: ['Content-Type', 'Authorization', 'auth-token', 'Origin', 'X-Requested-With', 'Accept'],// **Tilføj auth-token**
     credentials: true
