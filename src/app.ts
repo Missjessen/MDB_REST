7e0f1993bf73977275c0d03938e652bcc4c1de23
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-03-09 23:14:38 +0100

    swagger

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,9 @@ const app: Application = express();
 function corsSetup() {
     
 app.use(cors({
-   // origin: "http://localhost:5173",  // Tillad requests fra din frontend
-    origin: '*',  // Tillad requests fra din frontend
+    origin: '*', 
     methods: "GET,POST,PUT,DELETE,OPTIONS, HEAD", 
-    allowedHeaders: ['Content-Type', 'Authorization', 'auth-token', 'Origin', 'X-Requested-With', 'Accept'],// **Tilføj auth-token**
+    allowedHeaders: ['Content-Type', 'Authorization', 'auth-token', 'Origin', 'X-Requested-With', 'Accept'],
     credentials: true
 }))
 };
